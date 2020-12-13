a92fa20ffc35901c7e1a8f36b72ff63305cc857c
Maren Zoller <maren.zoller@gmail.com>
2020-12-13 21:43:36 +0100

    Add machine learning pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,31 +1,139 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import re
 
+import nltk
+nltk.download(['punkt', 'wordnet','stopwords'])
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+
+import pickle
 
 def load_data(database_filepath):
-    pass
+    """
+       Function:
+       load data from database
+
+       Args:
+       database_filepath: the path of the database
+
+       Return:
+       X (DataFrame) : Message features dataframe
+       Y (DataFrame) : target dataframe
+       category (list of str) : target labels list
+    """
+    # load data from database
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('disaster_messages_tbl', engine)
+    X = df['message']
+    Y = df[df.columns[4:]]
+    return X, Y
 
 
 def tokenize(text):
-    pass
+    """
+    Function: take in text data, normalize it, split it into words, remove stop words and return their root form
+
+    Args: 
+        text(str): text to be processed
+
+    Return:
+        lemm(list of str): a list of root forms of the words in the input text
+    """
+    # normalize text
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    words = word_tokenize(text)
+    
+    # remove stop words
+    stop = stopwords.words("english")
+    words = [t for t in words if t not in stop]
+    
+    # lemmatization
+    lemm = [WordNetLemmatizer().lemmatize(w) for w in words]
+    return lemm
 
 
 def build_model():
-    pass
+    """
+    Function: build a model to classify the disaster messages
+
+    Return:
+        cv(list of str): classification model
+    """
+    # create a pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf',  MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+
+    # create Grid search parameters
+    parameters = {
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [50, 60, 70]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model, X_test, Y_test):
+    """
+    Function: Evaluate the model and print the f1 score, precision and recall for each output category of the dataset.
+
+    Args:
+    model: the classification model
+
+    X_test: test messages
+    Y_test: test target
+    """
+    y_pred = model.predict(X_test)
+    i = 0
+
+    for col in Y_test:
+        print('Feature {}: {}'.format(i + 1, col))
+        print(classification_report(Y_test[col], y_pred[:, i]))
+        i = i + 1
+
+    accuracy = (y_pred == Y_test.values).mean()
+
+    print('The model accuracy is {:.3f}'.format(accuracy))
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    Function: Save a pickle file of the model
+
+    Args:
+    model: the classification model
+    model_filepath (str): the path of pickle file
+    """
+
+    # Create a pickle file for the model
+    with open (model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
+        X, Y = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
         print('Building model...')
@@ -35,7 +143,7 @@ def main():
         model.fit(X_train, Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, Y_test)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
